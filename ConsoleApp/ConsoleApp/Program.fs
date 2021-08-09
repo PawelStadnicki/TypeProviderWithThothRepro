9338496f391081f7a6196ccac0a23d9a10421f5d
Pawel Stadnicki <pawel.stadnicki@consultant.volvo.com>
2021-08-09 22:13:58 +0200

    Pushing repro projects

---
+++ ConsoleApp/ConsoleApp/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+type MyType = MyNamespace.MyType
+
+[<EntryPoint>]
+let main argv =
+    let mt = new MyType("My inner state")
+
+    printfn $"1. Inner state {mt.InnerState}"
+    printfn $"2. StaticMethod {MyType.StaticMethod()}"
+    printfn $"3. MathNumericsSampleValue {mt.MathNumericsSampleValue}"
+    //printfn $"4. ThothJsonSampleValue {mt.ThothJsonSampleValue}"
+    0 // return an integer exit code
